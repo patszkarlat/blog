385b0e9cfe7bf8bc6aaefad0509feb1445f557c8
patszkarlat <punkgeek@mac.com>
2014-09-08 16:04:27 -0400

    added udpating capabilities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,10 +18,27 @@ class ArticlesController < ApplicationController
 		end
 
 	end
-
+	
+	
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+    @article = Article.find (params[:id])
+
+    if @article.update(article_params)
+    	redirect_to @article
+    else
+    	render 'edit'
+    end
+  
+  end
+
 
 	private
 		def article_params
